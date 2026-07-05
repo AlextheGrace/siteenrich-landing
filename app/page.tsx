b93103a5c04227612560757fde62e332426e7dec
Alexander Grace <alexander.grace@visualart.com>
2026-07-05 23:08:35 +0200

    remove empty testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#0a0a0a] text-white">
       <Hero />
-      <TrustBar />
       <BeforeAfter />
       <PrivacyNote />
       <CsvUpload />
